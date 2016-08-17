eec6a5fa78e2ba8e9a1097eed2333f833efe491c
Maverick Chardet <chardetm@gmail.com>
2016-08-17 17:01:50 -0500

    Fixed missing ops in serializers read only fields

--- pmaapp/serializers.py
+++ pmaapp/serializers.py
@@ -9,8 +9,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = django.contrib.auth.get_user_model()
         fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_superuser', 'is_active',
-                  'date_joined', 'password',)
-        read_only_fields = ('id', 'is_staff', 'is_superuser', 'is_active', 'date_joined',)
+                  'date_joined', 'password', 'ops',)
+        read_only_fields = ('id', 'is_staff', 'is_superuser', 'is_active', 'date_joined', 'ops')
         extra_kwargs = {
             'password': {'write_only': True}
         }
